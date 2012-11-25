a956cf699729545dc580970bb2020cf8934f667a
Peter Burns <rictic@google.com>
2012-11-25 13:00:28 -0800

    add some definitions for angular

---
+++ app/lib/angular.d.ts
@@ -0,0 +1,29 @@
+declare module angular {
+  export var copy : (val:any)=>any;
+  export var module : (name:string, deps:string[])=>Module;
+  export interface Module {
+    factory(name:string, factImpl:Function);
+  }
+
+  export interface $http {
+    get(options:{}):$q.promise;
+  };
+
+  export module $q {
+    interface promise {
+      then(onSuccess:(arg:any)=>any, onFailure?:()=>any):promise;
+      get(property: string): promise;
+      done():void;
+    }
+    interface deferred {
+      resolve(arg:any);
+      reject(arg?:any);
+      promise:promise;
+    }
+  }
+
+  interface $q {
+    defer():$q.deferred;
+    resolve(val:any):$q.promise;
+  }
+}
