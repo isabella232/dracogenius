f9993cb2742c9bdbbb754b121239428dec026b89
Peter Burns <rictic@google.com>
2012-11-27 13:14:56 -0800

    added test runner to watch.sh

--- watch.sh
+++ watch.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 # requires inotify-tools: sudo apt-get install inotify-tools
 
+# Kill testacular when this script exits
+trap 'kill $(jobs -p)' EXIT
+
+testacular start &
+
 while :; do
   ./build.sh
   echo "Compile done."
